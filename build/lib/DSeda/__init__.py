94c601feb505a6b1d1641da84490df529fcdaffa
Cristian Tacoronte Rivero <cristiantr.develop@gmail.com>
2022-05-18 17:31:35 +0100

    configuración de prueba finalizada

---
+++ build/lib/DSeda/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 16:51:54 2022
+
+@author: crist
+"""
+
+def add_number(a,b):
+	return a+b
---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 16:56:24 2022
+
+@author: crist
+"""
+import setuptools
+setuptools.setup(
+	name='DSeda',
+	version='0.1',
+	author='Cristian Tacoronte Rivero',
+	author_email='cristiantr.develop@gmail.com',
+	description='''Libreria empleada para facilitar el proceso
+	EDA y demas procesos relacionados. Para mas informacion se puede
+	visitar el repositorio oficial <https://github.com/CristianTacoronteRivero/Lib_DSeda>
+	o enviar correo a cristiantr.develop@gmail.com''',
+	packages=setuptools.find_packages()
+	)
